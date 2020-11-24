aa67893d48b4fb658475a5c340affff4fdd9b230
Niskaaaa <1162314353@qq.com>
2020-11-24 23:39:51 +0800

    highlight init

---
+++ src/components/format/highlight.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+  <scroll-view scroll-x>
+  <block>
+  <view v-for="(item,index) in tokenLines" :key="index">
+<text class="line-number">{{i+index}}</text>
+</view>
+  </div>
+</template>
+
+<script type="text/javascript">
+
+export default {
+
+  data() {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
